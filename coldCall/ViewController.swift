34941aafcbe63aa8e51ea4f8d7003f09203b5da4
Mackenzie Wacker <wacker.mackenzierae@gmail.com>
2016-11-30 18:33:32 -0800

    FIRST COMMIT

--- coldCall/ViewController.swift
+++ coldCall/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var random: Int32 = 0
+    var names = ["Julia", "Kye", "Bryan", "Mackenzie", "Jimmy", "Brenda", "Anderson", "Justin"]
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var coldCallButton: UIButton!
+    
+    
+    @IBAction func coldCallButton(_ sender: UIButton) {
+        self.random = Int32(arc4random_uniform(8))
+        self.nameLabel.text = names[Int(random)]
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.nameLabel.text = "Ready?"
     }
 
     override func didReceiveMemoryWarning() {
